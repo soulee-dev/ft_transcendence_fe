74df90de8a9e400185908508b46b1c54138d6ea3
Soul Lee <alus20x@gmail.com>
2023-10-23 18:14:19 +0900

    fix: Add credential in fetch

--- src/app/2fa/[id]/page.js
+++ src/app/2fa/[id]/page.js
@@ -1,40 +1,46 @@
-'use client'
+"use client";
 
-import { useState, } from 'react'
+import { useState } from "react";
 
 export default function TwoFA({ params }) {
-	const [code, setCode] = useState('');
-	
-	const handleCodeChange = (event) => {
-		event.preventDefault();
-		setCode(event.target.value);
-	}
+  const [code, setCode] = useState("");
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		fetch('http://localhost:3000/auth/validate-otp', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				userId: params.id,
-				otp: code
-			})
-		})
-		.then(res => res.json())
-		.then(data => console.log(data))
-		.catch(error => console.error(error))
-	}
+  const handleCodeChange = (event) => {
+    event.preventDefault();
+    setCode(event.target.value);
+  };
 
-	return (
-		<div>
-			<h1>2FA</h1>
-			<h1>userId: { params.id }</h1>
-			<form onSubmit={handleSubmit}>
-				<input type="text" value={code} onChange={handleCodeChange} />
-				<button type="submit">Submit</button>
-			</form>
-		</div>
-	)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/auth/validate-otp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        userId: params.id,
+        otp: code,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.redirectURI) {
+          window.location = data.redirectURI;
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div>
+      <h1>2FA</h1>
+      <h1>userId: {params.id}</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={code} onChange={handleCodeChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
