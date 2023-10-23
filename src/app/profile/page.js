c5aaa9960f278d7f08d696a68ccbcbc7d678d9f9
Soul Lee <alus20x@gmail.com>
2023-10-23 15:54:31 +0900

    fix: Fully functioning my page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -5,7 +5,9 @@ import Cookies from "js-cookie";
 
 export default function Profile() {
   const [name, setName] = useState("");
-  const [data, setData] = useState({});
+  const [email, setEmail] = useState("");
+  const [is2fa, setIs2fa] = useState(false);
+  const [userData, setUserData] = useState({});
 
   const fetchUser = () => {
     const access_token = Cookies.get("access_token");
@@ -15,7 +17,12 @@ export default function Profile() {
       },
     })
       .then((res) => res.json())
-      .then((data) => setData(data))
+      .then((data) => {
+        setName(data.name);
+        setEmail(data.email);
+        setIs2fa(data.is_2fa);
+        setUserData(data);
+      })
       .catch((error) => console.error(error));
   };
 
@@ -24,34 +31,53 @@ export default function Profile() {
   const handleUpdate = (event) => {
     event.preventDefault();
     const access_token = Cookies.get("access_token");
-    const userData = { name };
+    const updateData = { name, email, is_2fa: is2fa };
     fetch("http://localhost:3000/users/me/update", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${access_token}`,
       },
-      body: JSON.stringify(userData),
+      body: JSON.stringify(updateData),
     })
       .then((res) => res.json())
       .then((data) => {
         fetchUser();
-        console.log(data);
       })
       .catch((error) => console.error(error));
   };
-
   return (
     <div>
-      <img src={data.profile_image} width={100} height={100}></img>
-      <p>안녕하세요 {data.name}님!</p>
+      <img src={userData.profile_image} width={100} height={100}></img>
       <form onSubmit={handleUpdate}>
+        <label htmlFor="name">이름</label>
+        <br />
         <input
           type="text"
+          id="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Name"
         />
+        <br />
+        <label htmlFor="is_2fa">이차인증 여부</label>
+        <br />
+        <input
+          type="checkbox"
+          id="is_2fa"
+          checked={is2fa}
+          onChange={(e) => setIs2fa(e.target.checked)}
+        />
+        <br />
+        <label htmlFor="email">이메일</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
         <button type="submit">저장하기</button>
       </form>
     </div>
