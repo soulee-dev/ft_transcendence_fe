245c0468f1c2e57fcb215a6aa8e7b8822200b8d7
Soul Lee <alus20x@gmail.com>
2023-10-24 20:27:04 +0900

    fix: Add links in left side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 import React from "react";
-import Link from "next/link";
-import Game from "../components/client/Game";
 
 export default function Home() {
 	return (
 		<div>
-			<ul>
-				<li><Link href="/profile">마이페이지</Link></li>
-				<li><Link href="/channels">채팅방</Link></li>
-				<li><Link href="/game">게임</Link></li>
-				<li><Link href="/friends">친구</Link></li>
-			</ul>
+			<h1>Main page</h1>
 		</div>
 	);
 }
--- src/components/server/LeftSide.tsx
+++ src/components/server/LeftSide.tsx
@@ -1,27 +1,14 @@
+import Link from "next/link";
+
 function LeftSide() {
 	return (
 		<div className="leftSide">
-			{/* <div className="leftSideList">
-				<div className="wrapper" onClick={goHome}>
-					<div className="leftSideBtn">홈</div>
-				</div>
-				<div className="container">
-					<div className="leftSideBtn">알림</div>
-				</div>
-				<div className="container">
-					<div className="leftSideBtn">친구 목록</div>
-				</div>
-				<div className="container">
-					<div className="leftSideBtn">채팅방</div>
-				</div>
-				<div className="container">
-					<div className="leftSideBtn">순위</div>
-				</div>
-				<div className="wrapper" onClick={blockList}>
-					<div className="leftSideBtn">차단 목록</div>
-				</div>
-			</div> */}
-			Left Side
+			<ul>
+				<li><Link href="/profile">마이페이지</Link></li>
+				<li><Link href="/channels">채팅방</Link></li>
+				<li><Link href="/game">게임</Link></li>
+				<li><Link href="/friends">친구</Link></li>
+			</ul>
 		</div>
 	);
 }
