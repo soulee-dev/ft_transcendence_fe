3e5860e9f3cfb74721e4df101b1cea08df1aaf56
Soul Lee <alus20x@gmail.com>
2023-10-24 20:23:11 +0900

    add: Friends link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
 				<li><Link href="/profile">마이페이지</Link></li>
 				<li><Link href="/channels">채팅방</Link></li>
 				<li><Link href="/game">게임</Link></li>
+				<li><Link href="/friends">친구</Link></li>
 			</ul>
 		</div>
 	);
