0da8ef7440eaaee16f66559bfe24785ff2157ff4
Soul Lee <alus20x@gmail.com>
2023-10-26 18:31:24 +0900

    fix: React Toastify to work

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
