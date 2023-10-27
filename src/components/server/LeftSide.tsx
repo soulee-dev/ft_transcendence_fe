8b63c22ab2154444042ca615503664fb0980bf0c
Soul Lee <alus20x@gmail.com>
2023-10-27 14:13:34 +0900

    add: Logout button

--- src/components/server/LeftSide.tsx
+++ src/components/server/LeftSide.tsx
@@ -1,24 +1,50 @@
+"use client";
+
 import Link from "next/link";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import Cookies from "js-cookie";
 
 function LeftSide() {
-	return (
-		<div className="leftSide">
-			<ul>
-				<li>
-					<Link href="/profile">마이페이지</Link>
-				</li>
-				<li>
-					<Link href="/channels">채팅방</Link>
-				</li>
-				<li>
-					<Link href="/game">게임</Link>
-				</li>
-				<li>
-					<Link href="/friends">친구</Link>
-				</li>
-			</ul>
-		</div>
-	);
+  const handleLogout = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    Cookies.remove("access_token");
+    toast.success(
+      <>
+        로그아웃 되었습니다.
+        <br />
+        잠시 후 메인페이지로 이동합니다.
+      </>
+    );
+    setTimeout(() => {
+      location.href = "/";
+    }, 1000);
+  };
+
+  return (
+    <div className="leftSide">
+      <ToastContainer />
+      <ul>
+        <li>
+          <Link href="/profile">마이페이지</Link>
+        </li>
+        <li>
+          <Link href="/channels">채팅방</Link>
+        </li>
+        <li>
+          <Link href="/game">게임</Link>
+        </li>
+        <li>
+          <Link href="/friends">친구</Link>
+        </li>
+        <li>
+          <a href="/" onClick={handleLogout}>
+            로그아웃
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default LeftSide;
