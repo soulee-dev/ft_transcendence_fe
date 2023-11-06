ecc06ba488ab2323cfef21b1b53318f0eed229f1
Soul Lee <alus20x@gmail.com>
2023-11-06 23:15:46 +0900

    Add useEffect hook to fetch user data and filter admin/owner channels

--- src/app/channel_admin/page.tsx
+++ src/app/channel_admin/page.tsx
@@ -5,41 +5,77 @@ import { ToastContainer, toast } from "react-toastify";
 import { useState } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { useEffect } from "react";
 
 export default function ChannelAdmin() {
-  const [channels, setChannels] = useState([]);
-  const [selectedChannel, setSelectedChannel] = useState(0);
+  const [userData, setUserDaa] = useState<any>({});
+  const [adminChannels, setAdminChannels] = useState([]);
 
-  const fetchChannels = () => {
+  useEffect(() => {
     const access_token = Cookies.get("access_token");
-
     axios
-      .get(`${process.env.NEXT_PUBLIC_API_URL}/channels/joined`, {
-        headers: { Authorization: `Bearer ${access_token}` },
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
       })
-      .then((res) => {
-        setChannels(res.data);
+      .then((response) => {
+        setUserDaa(response.data);
+        return axios.get(`${process.env.NEXT_PUBLIC_API_URL}/channels/joined`, {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        });
+      })
+      .then((response) => {
+        const channels = response.data;
+        const adminOrOwnerChannels: any = [];
+
+        channels.forEach((channel: any) => {
+          axios
+            .get(
+              `${process.env.NEXT_PUBLIC_API_URL}/channels/${channel.id}/users`,
+              {
+                headers: {
+                  Authorization: `Bearer ${access_token}`,
+                },
+              }
+            )
+            .then((userResponse) => {
+              const userAdminOrOwner = userResponse.data.find(
+                (user: any) =>
+                  user.user_id === userData.id && (user.admin || user.owner)
+              );
+
+              console.log(userAdminOrOwner);
+
+              if (userAdminOrOwner) {
+                adminOrOwnerChannels.push(channel);
+                setAdminChannels(adminOrOwnerChannels);
+              }
+            })
+            .catch((error) => {
+              toast.error(
+                (error.response?.data as { message: string })?.message
+              );
+              console.error(error);
+            });
+        });
       })
       .catch((error) => {
         toast.error((error.response?.data as { message: string })?.message);
         console.error(error);
       });
-  };
-
-  interface Channel {
-    id: number;
-    name: string;
-  }
+  }, []);
 
   return (
     <div>
-      <h1>채널 목록</h1>
-      {channels.map((channel: Channel) => (
-        <div key={channel.id}>
-          <h2>{channel.name}</h2>
-          <button onClick={() => setSelectedChannel(channel.id)}>선택</button>
-        </div>
-      ))}
+      <ToastContainer />
+      <ul>
+        {adminChannels.map((channel: any) => (
+          <li key={channel.id}>{channel.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
