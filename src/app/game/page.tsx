8e276383c205d7d6343982232ec9ec9540163eb0
Soul Lee <alus20x@gmail.com>
2023-11-09 18:53:28 +0900

    Fix socket.off call and add null check for roomId

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -228,6 +228,7 @@ export default function Game() {
 
     return () => {
       socket.off("declinedInvite");
+      if (!roomId) return;
       socket.emit("cancelMatch", roomId);
       socket.emit("leaveAsSpectator", roomId);
     };
